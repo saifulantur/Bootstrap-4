8c0991773a5da348b415d40affc78eb720e33694
saifulantur <40080527+saifulantur@users.noreply.github.com>
2019-08-08 11:51:52 +0600

    Add files via upload

--- starter-pack-bootstrap/gulpfile.js
+++ starter-pack-bootstrap/gulpfile.js
@@ -29,13 +29,13 @@ gulp.task('serve', ['sass'], function () {
 
 // Move Font Awesome Fonts folder to src
 gulp.task('webfonts', function () {
-    return gulp.src('node_modules/@fortawesome/fontawesome-free/webfonts/*')
-        .pipe(gulp.dest("src/webfonts"));
+    return gulp.src('node_modules/font-awesome/fonts/*')
+        .pipe(gulp.dest("src/fonts"));
 });
 
 // Move font awesome css file
 gulp.task('fa', function () {
-    return gulp.src('node_modules/@fortawesome/fontawesome-free/css/all.min.css')
+    return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
         .pipe(gulp.dest("src/css"));
 });
 
